9fc86030e80af3ba9c354d0c683f4dc7bbe72db1
kismatkc <kismatkc28@gmail.com>
2024-09-05 11:32:37 -0400

    auto

--- backend/controllers/get_best_podcast.ts
+++ backend/controllers/get_best_podcast.ts
@@ -4,61 +4,37 @@ import { Request, Response } from "express";
 import { Client } from "podcast-api";
 import { parseString } from "xml2js";
 import axios from "axios";
+import { json } from "stream/consumers";
+
 // const client = Client({ apiKey: process.env.LISTENNOTES_API_Key });
-const getTrendingPodcast =async (req: Request, res: Response) => {
-  
-let data = {}
-axios({
-  method: 'get',
-  url: "https://storage.googleapis.com/podcastemodata/hello.txt", // Replace with your URL
-  responseType: 'stream'
-})
-.then(response => {
-  let rawData = ''; // To accumulate chunks
+const getTrendingPodcast = async (req: Request, res: Response) => {
+  axios({
+    method: "get",
+    url: "https://storage.googleapis.com/podcastemodata/hello.txt", // Replace with your URL
+    responseType: "stream",
+  })
+    .then((response) => {
+      let rawData = ""; // To accumulate chunks
 
-  response.data.on('data', (chunk: any) => {
-    rawData += chunk; // Append each chunk of data to rawData
-  });
+      response.data.on("data", (chunk: any) => {
+        rawData += chunk; // Append each chunk of data to rawData
+      });
 
-  response.data.on('end', () => {
-    try {
-      const jsonObject = JSON.parse(rawData); // Convert the complete data to JSON
-      data = jsonObject;
-      console.log(data)
-      
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-    }
-  });
-
-})
-.catch(error => {
-  console.error('Error fetching stream:', error);
-});
-
-  
-
-   
-  // client
-  //   .fetchBestPodcasts({
-  //     genre_id: "93",
-  //     page: 2,
-  //     region: "us",
-  //     sort: "listen_score",
-  //     safe_mode: 0,
-  //   })
-  //   .then((response) => {
-  //     const bestPodcasts = response.data.podcasts;
-      
-  //     res.json(podcastXmlData);
-  //   })
-  //   .catch((error) => {
-  //     res.json(error);
-  //   });
-
-  
-  res.send(data);
-  return data;
+      response.data.on("end", () => {
+        try {
+          const bestPodcastsMetadata = JSON.parse(rawData); // Convert the complete data to JSON
+          const twentyPodcastObjects = bestPodcastsMetadata.podcasts;
+          const rssFeedUrls = twentyPodcastObjects.map((podcast) => {
+            return podcast.rss;
+          });
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching stream:", error);
+    });
 };
 
 export default getTrendingPodcast;
